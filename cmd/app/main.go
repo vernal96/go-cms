27aaf8ff00f63a99519f614c1d2950adfc82b59d
Денис Костенко <kostenko@morizo.ru>
2026-06-16 22:35:54 +0300

    init

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,14 +5,34 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/vernal96/go-cms/adapters/cache/memorycache"
+	"github.com/vernal96/go-cms/adapters/storage/memorystorage"
 	"github.com/vernal96/go-cms/core"
 	"github.com/vernal96/go-cms/internal/testsite"
 )
+
+const FileDiskMemory core.FileDisk = "memory"
 
 func main() {
 	ctx := context.Background()
 
 	app := core.NewApp()
+
+	if err := app.CacheManager().RegisterStore(core.CacheStoreMemory, memorycache.NewStore()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.CacheManager().SetDefaultStore(core.CacheStoreMemory); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.Storage().RegisterDisk(FileDiskMemory, memorystorage.NewStorage()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.Storage().SetDefaultDisk(FileDiskMemory); err != nil {
+		log.Fatal(err)
+	}
 
 	profileRegistry := core.NewDefaultSiteProfileRegistry()
 
