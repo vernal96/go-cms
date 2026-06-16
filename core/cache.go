bb99203f652b6341e3f92e376d2f402167f301c8
Денис Костенко <kostenko@morizo.ru>
2026-06-16 12:51:27 +0300

    init

--- core/cache.go
+++ core/cache.go
@@ -5,6 +5,14 @@ import (
 	"time"
 )
 
+const (
+	CacheStoreDefault = "default"
+	CacheStoreRedis   = "redis"
+	CacheStoreFile    = "file"
+	CacheStoreMemory  = "memory"
+	CacheStoreNull    = "null"
+)
+
 type CacheStore interface {
 	Get(ctx context.Context, key string) ([]byte, bool, error)
 	Set(ctx context.Context, key string, value []byte, ttl time.Duration) error
@@ -12,5 +20,10 @@ type CacheStore interface {
 }
 
 type CacheManager interface {
+	Default() CacheStore
+	Redis() CacheStore
+	File() CacheStore
+	Memory() CacheStore
+	Null() CacheStore
 	Store(name string) (CacheStore, bool)
 }
