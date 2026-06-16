4c1893a3931d33d790f0a039d97c6ef920370fed
Денис Костенко <kostenko@morizo.ru>
2026-06-16 23:17:40 +0300

    init

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,32 +5,16 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/vernal96/go-cms/adapters/cache/memorycache"
-	"github.com/vernal96/go-cms/adapters/storage/memorystorage"
 	"github.com/vernal96/go-cms/core"
+	"github.com/vernal96/go-cms/internal/project"
 	"github.com/vernal96/go-cms/internal/testsite"
 )
-
-const FileDiskMemory core.FileDisk = "memory"
 
 func main() {
 	ctx := context.Background()
 
-	app := core.NewApp()
-
-	if err := app.CacheManager().RegisterStore(core.CacheStoreMemory, memorycache.NewStore()); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := app.CacheManager().SetDefaultStore(core.CacheStoreMemory); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := app.Storage().RegisterDisk(FileDiskMemory, memorystorage.NewStorage()); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := app.Storage().SetDefaultDisk(FileDiskMemory); err != nil {
+	app, err := project.BootstrapApp()
+	if err != nil {
 		log.Fatal(err)
 	}
 
---
+++ internal/project/bootstrap.go
@@ -0,0 +1,38 @@
+package project
+
+import (
+	"log"
+
+	"github.com/vernal96/go-cms/adapters/cache/memorycache"
+	"github.com/vernal96/go-cms/adapters/eventbus/memoryeventbus"
+	"github.com/vernal96/go-cms/adapters/storage/memorystorage"
+	"github.com/vernal96/go-cms/core"
+)
+
+const FileDiskMemory core.FileDisk = "memory"
+
+func BootstrapApp() (*core.App, error) {
+	app := core.NewApp()
+
+	if err := app.CacheManager().RegisterStore(core.CacheStoreMemory, memorycache.NewStore()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.CacheManager().SetDefaultStore(core.CacheStoreMemory); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.Storage().RegisterDisk(FileDiskMemory, memorystorage.NewStorage()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.Storage().SetDefaultDisk(FileDiskMemory); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.SetEventBus(memoryeventbus.NewBus()); err != nil {
+		return nil, err
+	}
+
+	return app, nil
+}
