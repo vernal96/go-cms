55fbe102c34144786918296a69badadbdf487b77
Денис Костенко <kostenko@morizo.ru>
2026-06-16 22:54:20 +0300

    init

--- core/app.go
+++ core/app.go
@@ -1,14 +1,18 @@
 package core
 
+import "errors"
+
 type App struct {
 	cache   CacheManager
 	storage FileStorageManager
+	events  EventBus
 }
 
 func NewApp() *App {
 	return &App{
 		cache:   NewDefaultCacheManager(),
 		storage: NewDefaultFileStorageManager(),
+		events:  NullEventBus{},
 	}
 }
 
@@ -18,4 +22,18 @@ func (a *App) CacheManager() CacheManager {
 
 func (a *App) Storage() FileStorageManager {
 	return a.storage
+}
+
+func (a *App) EventBus() EventBus {
+	return a.events
+}
+
+func (a *App) SetEventBus(bus EventBus) error {
+	if bus == nil {
+		return errors.New("event bus is nil")
+	}
+
+	a.events = bus
+
+	return nil
 }
