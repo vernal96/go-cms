99b8517e016ab029e101a3e7c6b294357f06f636
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-06-17 23:50:26 +0300

    Add infrastructure config

---
+++ internal/project/infrastructure_config.go
@@ -0,0 +1,19 @@
+package project
+
+import "github.com/vernal96/go-cms/core"
+
+type CacheStoreRegistration struct {
+	Name  core.CacheStoreName
+	Store core.CacheStore
+}
+
+type FileDiskRegistration struct {
+	Name    core.FileDisk
+	Storage core.FileStorage
+}
+
+type InfrastructureConfig struct {
+	CacheStores []CacheStoreRegistration
+	FileDisks   []FileDiskRegistration
+	Events      core.EventBus
+}
