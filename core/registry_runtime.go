bd1d37ea379642cf84b59ad2d29e7590e9bb23c9
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-06-23 15:20:10 +0300

    Add runtime registry implementation

---
+++ core/registry_runtime.go
@@ -0,0 +1,39 @@
+package core
+
+type RuntimeRegistry struct {
+	moduleCode      string
+	widgets         map[WidgetCode]Widget
+	widgetTemplates map[WidgetCode]map[WidgetTemplateCode]WidgetTemplate
+}
+
+func NewRuntimeRegistry() *RuntimeRegistry {
+	return &RuntimeRegistry{
+		widgets:         make(map[WidgetCode]Widget),
+		widgetTemplates: make(map[WidgetCode]map[WidgetTemplateCode]WidgetTemplate),
+	}
+}
+
+func (r *RuntimeRegistry) ForModule(moduleCode string) Registry {
+	return &RuntimeRegistry{
+		moduleCode:      moduleCode,
+		widgets:         r.widgets,
+		widgetTemplates: r.widgetTemplates,
+	}
+}
+
+func (r *RuntimeRegistry) Widgets() WidgetRegistry {
+	return &runtimeWidgetRegistry{
+		moduleCode: r.moduleCode,
+		widgets:    r.widgets,
+	}
+}
+
+func (r *RuntimeRegistry) WidgetTemplates() WidgetTemplateRegistry {
+	return &runtimeWidgetTemplateRegistry{
+		moduleCode:      r.moduleCode,
+		widgets:         r.widgets,
+		widgetTemplates: r.widgetTemplates,
+	}
+}
+
+var _ Registry = (*RuntimeRegistry)(nil)
