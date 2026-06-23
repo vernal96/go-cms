1b4ed93af9db19cf421bd17c7412128957297a2f
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-06-23 20:34:56 +0300

    Add controller storage to runtime registry

--- core/registry_runtime.go
+++ core/registry_runtime.go
@@ -1,43 +1,54 @@
 package core
 
 type RuntimeRegistry struct {
-	moduleCode      string
-	widgets         map[WidgetCode]Widget
-	widgetTemplates map[WidgetCode]map[WidgetTemplateCode]WidgetTemplate
+	moduleCode string
+	state      *runtimeRegistryState
 }
 
-func (r *RuntimeRegistry) Controllers() ControllerRegistry {
-	//TODO implement me
-	panic("implement me")
+type runtimeRegistryState struct {
+	widgets         map[WidgetCode]Widget
+	widgetTemplates map[WidgetCode]map[WidgetTemplateCode]WidgetTemplate
+	controllers     []Controller
+	controllerRoutes map[string]struct{}
 }
 
 func NewRuntimeRegistry() *RuntimeRegistry {
 	return &RuntimeRegistry{
-		widgets:         make(map[WidgetCode]Widget),
-		widgetTemplates: make(map[WidgetCode]map[WidgetTemplateCode]WidgetTemplate),
+		state: &runtimeRegistryState{
+			widgets:          make(map[WidgetCode]Widget),
+			widgetTemplates:  make(map[WidgetCode]map[WidgetTemplateCode]WidgetTemplate),
+			controllers:      make([]Controller, 0),
+			controllerRoutes: make(map[string]struct{}),
+		},
 	}
 }
 
 func (r *RuntimeRegistry) ForModule(moduleCode string) Registry {
 	return &RuntimeRegistry{
-		moduleCode:      moduleCode,
-		widgets:         r.widgets,
-		widgetTemplates: r.widgetTemplates,
+		moduleCode: moduleCode,
+		state:      r.state,
 	}
 }
 
 func (r *RuntimeRegistry) Widgets() WidgetRegistry {
 	return &runtimeWidgetRegistry{
 		moduleCode: r.moduleCode,
-		widgets:    r.widgets,
+		widgets:    r.state.widgets,
 	}
 }
 
 func (r *RuntimeRegistry) WidgetTemplates() WidgetTemplateRegistry {
 	return &runtimeWidgetTemplateRegistry{
 		moduleCode:      r.moduleCode,
-		widgets:         r.widgets,
-		widgetTemplates: r.widgetTemplates,
+		widgets:         r.state.widgets,
+		widgetTemplates: r.state.widgetTemplates,
+	}
+}
+
+func (r *RuntimeRegistry) Controllers() ControllerRegistry {
+	return &runtimeControllerRegistry{
+		controllers:      &r.state.controllers,
+		controllerRoutes: r.state.controllerRoutes,
 	}
 }
 
