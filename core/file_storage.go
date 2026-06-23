dad183f2d27706994ab5a63d6329e3f61d676ed0
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-06-23 15:20:56 +0300

    Refactor file storage contracts

--- core/file_storage.go
+++ core/file_storage.go
@@ -20,21 +20,3 @@ type FileStorage interface {
 type FileStorageManager interface {
 	Disk(name FileDisk) (FileStorage, error)
 }
-
-type NullFileStorage struct{}
-
-func (s NullFileStorage) Save(ctx context.Context, path string, content io.Reader) error {
-	return nil
-}
-
-func (s NullFileStorage) Open(ctx context.Context, path string) (io.ReadCloser, error) {
-	return nil, ErrFileNotFound
-}
-
-func (s NullFileStorage) Delete(ctx context.Context, path string) error {
-	return nil
-}
-
-func (s NullFileStorage) Exists(ctx context.Context, path string) (bool, error) {
-	return false, nil
-}
