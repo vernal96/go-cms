bfdfd239804a84e156337dbd9dfb2dd74cd316dd
Денис Костенко <kostenko@morizo.ru>
2026-07-06 19:42:38 +0300

    init

--- kernel/site_runtime_factory.go
+++ kernel/site_runtime_factory.go
@@ -16,9 +16,7 @@ func NewSiteRuntimeFactory(app *App) *SiteRuntimeFactory {
 func (f *SiteRuntimeFactory) Make(ctx context.Context, profile Profile) (*SiteRuntime, error) {
 	registry := NewRuntimeRegistry()
 
-	modules := profile.Modules()
-
-	for _, module := range modules {
+	for _, module := range profile.Modules() {
 		moduleRegistry := registry.ForModule(module.Code())
 
 		if err := module.Register(moduleRegistry); err != nil {
@@ -29,7 +27,7 @@ func (f *SiteRuntimeFactory) Make(ctx context.Context, profile Profile) (*SiteRu
 	runtime := NewSiteRuntime(f.app, profile, registry)
 	moduleContext := NewModuleContext(f.app)
 
-	for _, module := range modules {
+	for _, module := range profile.Modules() {
 		if err := module.Boot(ctx, moduleContext); err != nil {
 			return nil, fmt.Errorf("boot module %q: %w", module.Code(), err)
 		}
