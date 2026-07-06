662e091ce07fbdca931ea6cebdc52ea722dbfa1b
Денис Костенко <kostenko@morizo.ru>
2026-07-06 18:13:52 +0300

    init

---
+++ core/site_runtime.go
@@ -0,0 +1,27 @@
+package core
+
+type SiteRuntime struct {
+	app      *App
+	profile  Profile
+	registry Registry
+}
+
+func NewSiteRuntime(app *App, profile Profile, registry Registry) *SiteRuntime {
+	return &SiteRuntime{
+		app:      app,
+		profile:  profile,
+		registry: registry,
+	}
+}
+
+func (r *SiteRuntime) App() *App {
+	return r.app
+}
+
+func (r *SiteRuntime) Profile() Profile {
+	return r.profile
+}
+
+func (r *SiteRuntime) Registry() Registry {
+	return r.registry
+}
