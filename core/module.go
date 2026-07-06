d05708f0f9802c4ede80e69c92c9fd1a54127dec
Денис Костенко <kostenko@morizo.ru>
2026-07-06 17:37:28 +0300

    init

--- core/module.go
+++ core/module.go
@@ -7,5 +7,5 @@ type ModuleCode string
 type Module interface {
 	Code() ModuleCode
 	Register(registry Registry) error
-	Boot(ctx context.Context, app *App) error
+	Boot(ctx context.Context, moduleContext ModuleContext) error
 }
---
+++ core/module_context.go
@@ -0,0 +1,13 @@
+package core
+
+type ModuleContext struct {
+	app *App
+}
+
+func NewModuleContext(app *App) *ModuleContext {
+	return &ModuleContext{app: app}
+}
+
+func (c *ModuleContext) App() *App {
+	return c.app
+}
