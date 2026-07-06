9090215487189f374aefef109caf2b884c74b854
Денис Костенко <kostenko@morizo.ru>
2026-07-06 21:55:18 +0300

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/vernal96/go-cms
 
 go 1.26.1
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Server ServerConfig
+}
+
+type ServerConfig struct {
+	Host            string        `envconfig:"SERVER_HOST" default:""`
+	Port            int           `envconfig:"SERVER_PORT" default:"8080"`
+	ReadTimeout     time.Duration `envconfig:"SERVER_READ_TIMEOUT" default:"5s"`
+	WriteTimeout    time.Duration `envconfig:"SERVER_READ_TIMEOUT" default:"10s"`
+	IdleTimeout     time.Duration `envconfig:"SERVER_READ_TIMEOUT" default:"120s"`
+	ShutdownTimeout time.Duration `envconfig:"SERVER_READ_TIMEOUT" default:"5s"`
+}
+
+func (c ServerConfig) Address() string {
+	return net.JoinHostPort(c.Host, strconv.Itoa(c.Port))
+}
+
+func Load() (*Config, error) {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
