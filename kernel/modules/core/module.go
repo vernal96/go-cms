f8b692ca64dcdd6de6844f440a1296139c22f7c5
Денис Костенко <kostenko@morizo.ru>
2026-07-06 19:10:41 +0300

    init

--- kernel/modules/core/module.go
+++ kernel/modules/core/module.go
@@ -1,9 +1,23 @@
 package core
 
-import "github.com/vernal96/go-cms/kernel"
+import (
+	"context"
+
+	"github.com/vernal96/go-cms/kernel"
+)
 
 type Module struct{}
 
 func (m Module) Code() kernel.ModuleCode {
 	return "core"
 }
+
+func (m Module) Register(registry kernel.Registry) error {
+	return nil
+}
+
+func (m Module) Boot(ctx context.Context, moduleContext kernel.ModuleContext) error {
+	return ctx.Err()
+}
+
+var _ kernel.Module = Module{}
