53cb063163ec38a8b4152e38edb092de2dfc5102
Денис Костенко <kostenko@morizo.ru>
2026-07-06 19:51:46 +0300

    init

--- kernel/modules/core/site/resolver.go
+++ kernel/modules/core/site/resolver.go
@@ -1,6 +1,9 @@
 package site
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type Resolver interface {
 	ResolveByDomain(ctx context.Context, domain string) (Site, bool, error)
@@ -10,13 +13,20 @@ type RepositoryResolver struct {
 	repository Repository
 }
 
-func NewRepositoryResolver(repository Repository) *RepositoryResolver {
+func NewRepositoryResolver(repository Repository) (*RepositoryResolver, error) {
+	if repository == nil {
+		return nil, errors.New("site repository is nil")
+	}
 	return &RepositoryResolver{
 		repository: repository,
-	}
+	}, nil
 }
 
 func (r *RepositoryResolver) ResolveByDomain(ctx context.Context, domain string) (Site, bool, error) {
+	if domain == "" {
+		return Site{}, false, errors.New("site domain is empty")
+	}
+
 	return r.repository.FindByDomain(ctx, domain)
 }
 
