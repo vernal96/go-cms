4f03c605b35326b581a50d574d6469a92a743109
Денис Костенко <kostenko@morizo.ru>
2026-07-06 16:45:35 +0300

    init

--- core/app.go
+++ core/app.go
@@ -1,13 +1,10 @@
 package core
 
-// App is the root CMS application container.
-//
-// At this stage it is intentionally empty. Later it will hold application-level
-// dependencies such as logger, cache manager, storage manager, event bus,
-// repositories, and configuration.
+type AppConfig struct {
+}
+
 type App struct{}
 
-// NewApp creates a new CMS application container.
-func NewApp() *App {
+func NewApp(config AppConfig) *App {
 	return &App{}
 }
