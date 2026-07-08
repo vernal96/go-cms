91f0af40b49706c498a650ce6a323651abeb6996
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-07-08 13:22:35 +0300

    Use cascaded adapter defaults in core config

--- kernel/modules/core/config.go
+++ kernel/modules/core/config.go
@@ -3,9 +3,20 @@ package core
 import "github.com/vernal96/go-cms/kernel"
 
 type Config struct {
-	Site SiteConfig
+	AdapterDefaults kernel.AdapterDefaults
+	Site            SiteConfig
 }
 
 type SiteConfig struct {
-	RepositoryAdapter kernel.AdapterCode `default:"postgres"`
+	RepositoryAdapter kernel.AdapterCode
+}
+
+func (c Config) SiteAdapterDefaults(parent kernel.AdapterDefaults) kernel.AdapterDefaults {
+	return kernel.ResolveAdapterDefaults(
+		parent,
+		c.AdapterDefaults,
+		kernel.AdapterDefaults{
+			RepositoryAdapter: c.Site.RepositoryAdapter,
+		},
+	)
 }
