5d8ad3197b84a78d908bd747e6efe4cf9d688a93
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-07-08 23:36:28 +0300

    Declare site repository adapter contract

--- kernel/modules/core/site/repository.go
+++ kernel/modules/core/site/repository.go
@@ -1,6 +1,12 @@
 package site
 
-import "context"
+import (
+	"context"
+
+	"github.com/vernal96/go-cms/kernel"
+)
+
+const RepositoryAdapterContract kernel.AdapterContractCode = "core.site.repository"
 
 type Repository interface {
 	FindByDomain(ctx context.Context, domain string) (Site, bool, error)
