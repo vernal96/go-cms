163dfaf4d13bb746e00f273ad30f8f771dd54409
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-07-08 23:36:55 +0300

    Use built-in default adapter fallback in project bootstrap

--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -3,11 +3,7 @@ package bootstrap
 import "github.com/vernal96/go-cms/kernel"
 
 func NewApp() (*kernel.App, error) {
-	app := kernel.NewApp(kernel.AppConfig{
-		AdapterDefaults: kernel.AdapterDefaults{
-			RepositoryAdapter: "postgres",
-		},
-	})
+	app := kernel.NewApp(kernel.AppConfig{})
 
 	return app, nil
 }
