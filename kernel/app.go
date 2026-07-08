71387f6bdc54a6633232004f13a7643d22cc683a
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-07-08 13:22:04 +0300

    Store adapter defaults in app config

--- kernel/app.go
+++ kernel/app.go
@@ -1,10 +1,23 @@
 package kernel
 
 type AppConfig struct {
+	AdapterDefaults AdapterDefaults
 }
 
-type App struct{}
+type App struct {
+	config AppConfig
+}
 
 func NewApp(config AppConfig) *App {
-	return &App{}
+	return &App{
+		config: config,
+	}
+}
+
+func (a *App) Config() AppConfig {
+	return a.config
+}
+
+func (a *App) AdapterDefaults() AdapterDefaults {
+	return ResolveAdapterDefaults(a.config.AdapterDefaults)
 }
