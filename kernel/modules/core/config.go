97b57f08c29676f049a45783817da1a7c6ffe4c0
vernal96 <90603566+vernal96@users.noreply.github.com>
2026-07-15 10:54:54 +0300

    Restore explicit core module config

--- kernel/modules/core/config.go
+++ kernel/modules/core/config.go
@@ -3,11 +3,9 @@ package core
 import "github.com/vernal96/go-cms/kernel"
 
 type Config struct {
-	AdapterDefaults kernel.AdapterDefaults
-	Site            SiteConfig
+	Site SiteConfig
 }
 
 type SiteConfig struct {
-	AdapterDefaults   kernel.AdapterDefaults
-	RepositoryAdapter kernel.AdapterCode `cms:"adapter,contract=core.site.repository,default=repository"`
+	RepositoryAdapter kernel.AdapterCode
 }
