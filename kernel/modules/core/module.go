3e7f7707954e8402514cd1967cd0108fd6040c10
Денис Костенко <kostenko@morizo.ru>
2026-07-15 16:18:46 +0300

    init

--- kernel/modules/core/module.go
+++ kernel/modules/core/module.go
@@ -2,10 +2,8 @@ package core
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/vernal96/go-cms/kernel"
-	"github.com/vernal96/go-cms/kernel/modules/core/site"
 )
 
 const ModuleCode kernel.ModuleCode = "core"
@@ -21,27 +19,7 @@ func (m Module) Register(registry kernel.Registry) error {
 }
 
 func (m Module) Boot(ctx context.Context, moduleContext kernel.ModuleContext) error {
-	moduleConfig, err := kernel.ModuleConfigFrom[Config](moduleContext)
-	if err != nil {
-		return err
-	}
-
-	if moduleConfig.Site.RepositoryAdapter == "" {
-		return fmt.Errorf("core site repository adapter is not configured")
-	}
-
-	siteRepository, err := kernel.AdapterAs[site.Repository](
-		moduleContext.App().Adapters(),
-		site.RepositoryAdapterContract,
-		moduleConfig.Site.RepositoryAdapter,
-	)
-	if err != nil {
-		return err
-	}
-
-	_ = siteRepository
-
-	return ctx.Err()
+	return nil
 }
 
 var _ kernel.Module = Module{}
